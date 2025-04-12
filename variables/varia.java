cc3c519647aae64cda36889bde8c97f51aa56dd3
akshu88 <akshitaag904@gmail.com>
2025-04-12 07:10:54 +0530

    variable.java

---
+++ variables/varia.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class varia {
+    public static void main(String[]args){
+        String name = "Bro code";
+        int gta = 6;
+        double pi = 3.14159;
+        char gender = 'M';
+        boolean isAdmin = true;
+
+        System.out.println(name);
+        System.out.println(gta);
+        System.out.println(pi);
+        System.out.println(gender);
+        System.out.println(isAdmin);
+    }
+
+}
