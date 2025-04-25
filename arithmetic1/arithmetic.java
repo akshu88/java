027054097404c3ddab889b4138b877edace89a7d
akshu88 <akshitaag904@gmail.com>
2025-04-26 01:26:21 +0530

    arithmetic1.java

---
+++ arithmetic1/arithmetic.java
@@ -0,0 +1,47 @@
+package arithmetic1;
+
+public class arithmetic {
+    public static void main(String[]args){
+
+        //ARITHMETIC OPERATIONS
+
+    //    int x = 10;
+    //    int y = 4;
+      //  int z;
+
+      //  z = x + y;
+      //  z = x - y;
+      //  z = x * y;
+      //  z = x / y;
+      //  z = x % y;
+      //  System.out.println(z);
+
+      //AUGMENTED ASSIGNMENT OPERATOR
+    //  int x = 10;
+    //  int y = 2;
+
+     // x = x + y;
+     // x += y;
+     // x = x - y;
+     // x -= y;
+    //  x = x * y;
+    //  x *= y;
+    //  x = x / y;
+    //  x /= y;
+     // x = x % y;
+     // x %= y;
+     // System.out.println(x);
+
+
+     //INCREMENT AND DECREMENT OPERATOR
+    // int x = 1;
+    // x++;
+    // x--;
+
+    // System.out.println(x);
+
+
+//    ORDER OF OPERATION (P-E-M-D-A-S)
+        
+    }
+}
