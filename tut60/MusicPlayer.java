64f83ef76f1a26984f7df32deea28b9b7412d149
akshu88 <akshitaag904@gmail.com>
2025-06-01 23:38:04 +0530

    Implement MusicPlayer class to play audio files; include controls for play, stop, reset, and quit with exception handling.

---
+++ tut60/MusicPlayer.java
@@ -0,0 +1,77 @@
+package tut60;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class MusicPlayer {
+    public static void main(String[] args) {
+        // How to PLAY AUDIO with Java (.wav, .au, .aiff)
+
+        String filePath = "C:\\Users\\dell\\OneDrive\\Desktop\\GITHUB\\java\\tut60\\Glass Chinchilla - The Mini Vandals.wav";
+        File file = new File(filePath);
+
+        try(Scanner scanner = new Scanner(System.in); 
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+            
+
+            String response = "";
+
+            while (!response.equals("Q")) {
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Rset");
+                System.out.println("Q = Quit");
+                System.out.print("Enter your choice: ");
+
+                response = scanner.next().toUpperCase();
+
+                switch (response) {
+                    case "P":
+                        clip.start();
+                        break;
+                    case "S":
+                        clip.stop();
+                        break;
+                    case "R":
+                        clip.setMicrosecondPosition(0);
+                        break;
+                    case "Q":
+                        clip.close();
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                        break;
+                }
+
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch(UnsupportedAudioFileException e){
+            System.out.println("Audio file is not supported");
+        }
+        catch(LineUnavailableException e){
+            System.out.println("Unable to access audio resource");
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong");
+        }
+        finally{
+            System.out.println("BYE!");
+        }
+
+    }
+}
